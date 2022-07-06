513ec32d0c9a403e69c5c3a26c650e2c5fc1242a
McTyler Tong <mctylert245@gmail.com>
2022-07-06 17:30:49 -0500

    5th edit

--- server/src/app.js
+++ server/src/app.js
@@ -13,6 +13,9 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use(planetsRouter)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = {
     app
