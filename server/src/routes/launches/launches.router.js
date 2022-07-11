4b9590bc6248d41bc01cb41608bde78c9d00ee81
McTyler Tong <mctylert245@gmail.com>
2022-07-11 12:36:25 -0500

    9th edit

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const {
-    httpGetAllLaunches
+    httpGetAllLaunches,
+    httpAddNewLaunch,
+    httpAbortLaunch,
 } = require('./launches.controller')
 
 const launchesRouter = express.Router()
 
-launchesRouter.get('/launches', httpGetAllLaunches)
+launchesRouter.get('/', httpGetAllLaunches)
+launchesRouter.post('/', httpAddNewLaunch)
+launchesRouter.delete('/:id', httpAbortLaunch)
 
 module.exports = launchesRouter
